5ff10241091d8b7944f43cc9e4ef9081b9592e90
Percepco Ltd <43350524-admin2886@users.noreply.replit.com>
2025-06-01 01:08:15 +0000

    Pre-Recovery checkpoint

--- replit.nix
+++ replit.nix
@@ -7,5 +7,21 @@
     pkgs.pango
     pkgs.cairo
     pkgs.jq
+    pkgs.glib
+    pkgs.glibc
+    pkgs.libnss3
+    pkgs.libX11
+    pkgs.libXcomposite
+    pkgs.libXcursor
+    pkgs.libXdamage
+    pkgs.libXrandr
+    pkgs.libXss
+    pkgs.libXtst
+    pkgs.cups
+    pkgs.atk
+    pkgs.libasound
+    pkgs.gbm
+    pkgs.gtk3
+    pkgs.libXshmfence
   ];
 }
